babf62a41af9f6e995224f3de6da1778b0ff0a3e
samuelGrontoski <contatosamuelgrontoski@gmail.com>
2024-03-27 13:31:10 -0300

    Att

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Vite + React + TypeScript + Tailwind CSS + Python + Flask + MySQL
 
-Este repositório contem a integração entre as seguintes tecnologias:
-  - Front-end: Vite + React + TypeScript + Tailwind CSS
-  - Back-end: Python + Flask
-  - Database: MySQL
+  ## - Front-end: Vite + React + TypeScript + Tailwind CSS
+  ## - Back-end: Python + Flask
+  ## - Database: MySQL
+
--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,33 +1,16 @@
-import { useState, useEffect } from "react";
-import axios from 'axios';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Login } from './pages/page_login';
+import { Chamados } from './pages/page_chamados';
 
 export function App() {
-  const [array, setArray] = useState([]);
-
-  const fetchAPI = async () => {
-    const response = await axios.get("http://localhost:8080/api/users");
-    setArray(response.data.users);
-  };
-
-  useEffect(() => {
-    fetchAPI();
-  }, []);
 
   return (
-    <h1 className="text-cinza-light font-bold underline">
-      Hello World
-      <div className="content">
-        <p>
-          {
-            array.map((user, index) => (
-              <div key={index}>
-                <span>{user}</span>
-                <br></br>
-              </div>
-            ))
-          }
-        </p>
-      </div>
-    </h1>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>} />
+        <Route path="/chamados" element={<Chamados/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
