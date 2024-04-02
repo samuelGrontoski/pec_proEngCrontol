9834ad1eab6cf2a8f0adfcaef444fe5ac6eea68c
samuelGrontoski <contatosamuelgrontoski@gmail.com>
2024-04-02 11:13:44 -0300

    att

--- front-end/src/pages/page_chamados.tsx
+++ front-end/src/pages/page_chamados.tsx
@@ -3,21 +3,12 @@ import { Link } from 'react-router-dom';
 import { Sidebar } from '../components/sidebar';
 import { BarraPesquisa } from '../components/barra_pesquisa';
 import { useEffect, useState } from 'react';
-<<<<<<< HEAD
 import { CollapsibleTable } from "../components/collapsible_table"
 
-=======
-import { MainView } from '../components/main_view';
-import Table from "../components/table";
->>>>>>> 769c76b2e8f0543495a99bd2882f0065bb94177c
 
 export function Chamados() {
     const [dados, setDados] = useState([]);
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 769c76b2e8f0543495a99bd2882f0065bb94177c
     useEffect(() => {
         const intervalId = setInterval(fetchData, 3000); // Busca os dados a cada 1 minuto
         return () => clearInterval(intervalId); // Limpa o temporizador quando o componente é desmontado
@@ -47,7 +38,6 @@ export function Chamados() {
             console.error("Erro fetching dados: ", error)
         }
     };
-<<<<<<< HEAD
 
     return (
         <div className="bg-cinza-200 w-full h-full">
@@ -55,31 +45,17 @@ export function Chamados() {
                 <div className="bg-pec fixed inset-y-0 left-0 w-[200px]">
                     <Sidebar />
                 </div>
-=======
-    return (
-        <div>
-            <Background />
-            <body>
-                <Sidebar />
->>>>>>> 769c76b2e8f0543495a99bd2882f0065bb94177c
                 <main className='absolute inset-y-0 right-0 w-10/12 grid gap-y-5 p-10'>
                     <BarraPesquisa />
                     <div className='w-9/12 text-cinza-500'>
                         <p className='text-sm'>Dashboard Chamados</p>
                         <p className='text-base'>Olá, @usuário</p>
                     </div>
-<<<<<<< HEAD
                     <div>
                         <div className="py-5">
                             <CollapsibleTable />
                         </div>
                         {/* <div className='grid justify-items-center p-5'>
-=======
-                    <MainView className='flex justify-items-center'>
-                        <div>
-                        </div>
-                        <div className='grid justify-items-center p-5'>
->>>>>>> 769c76b2e8f0543495a99bd2882f0065bb94177c
                             <table className="table-fixed table-xs w-11/12 md:w-full border-collapse text-xs">
                                 <thead>
                                     <tr className='bg-table_header'>
@@ -114,13 +90,8 @@ export function Chamados() {
                                     ))}
                                 </tbody>
                             </table>
-<<<<<<< HEAD
                         </div> */}
                     </div>
-=======
-                        </div>
-                    </MainView>
->>>>>>> 769c76b2e8f0543495a99bd2882f0065bb94177c
                 </main>
             </body>
 
